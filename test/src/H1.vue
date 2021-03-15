b98a2b0491df438925a90c43396945216a344cbe
Gregory Quinio <1063530+gqio@users.noreply.github.com>
2021-03-15 23:43:29 +0100

    asdf (#1)

---
+++ test/src/H1.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1 style="background: teal;color:white">
+    <slot></slot>
+  </h1>
+  <details v-if="$attrs.showComponents">
+    <summary>mdxComponents</summary>
+    <pre>{{ $mdxComponents }}</pre>
+  </details>
+</template>
+
+<script>
+  export default {
+  inject: ['$mdxComponents'],
+}
+</script>
---
+++ test/src/Layout.vue
@@ -0,0 +1,26 @@
+<template>
+  <div style="border:1px solid red;width:400px">
+    <MDXProvider :mdxComponents="mdxComponents">
+      <slot></slot>
+    </MDXProvider>
+  </div>
+</template>
+
+<script>
+  import MDXProvider from './MDXProvider.vue';
+  import H1 from './H1.vue';
+  import Hello from './Hello.vue'
+  import Counter from './Counter.vue';
+  const mdxComponents = {
+  h1: H1,
+  "my-counter" : Counter,
+  Counter : Counter,
+  Hello
+};
+  export default {
+  components: {MDXProvider},
+  setup(){
+    return { mdxComponents }
+  }
+}
+</script>
